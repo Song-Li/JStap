da7e8d4af06c8a561d94f6044843e8bac2bd1787
Song-Li <lisong1013@gmail.com>
2020-02-20 18:59:16 -0500

    doing

--- pdg_generation/build_cpg_csv.py
+++ pdg_generation/build_cpg_csv.py
@@ -1,33 +1,89 @@
-import pickle
+"""
+This program is mainly used for generating the dfg and convert the dfg to csv
+which can be used later
+"""
 from pdgs_generation import *
 import argparse
 from os.path import isfile, join
 from os import listdir
 
-class Loader:
+class DFG_generator:
     def __init__(self, file_dir):
         print(file_dir)
-        self.pickles = []
-        store_pdg_folder(file_dir)
-        pickle_path = "{}/Analysis/PDG/".format(file_dir)
-        pickle_pathes = [join(pickle_path, f) for f in listdir(pickle_path) if isfile(join(pickle_path, f))]
-        for cur_pickle in pickle_pathes:
-            print(cur_pickle)
-            self.pickles.append(pickle.load(open(cur_pickle, 'rb')))
-        for cur_pickle in self.pickles:
-            self.traversal_pickle(cur_pickle, 0)
+        file_pathes = [join(file_dir, f) for f in listdir(file_dir) 
+                if isfile(join(file_dir, f)) and os.path.splitext(f)[1] == '.js']
+        for file_path in file_pathes:
+            pdg = get_data_flow(file_path, dict())
+            self.traversal(pdg, 0, monitor=self.check_sink)
 
-    def traversal_pickle(self, node, level):
-        print("\t" * level, node.attributes, node.data_dep_children)
+    def traversal(self, node, level, monitor=None):
+        """
+        traversal a tree from the root
+        Args:
+            node: the node to start
+            monitor: for each node, triger the monitor function
+        """
+        if monitor is not None:
+            monitor(node)
+        # print("\t" * level, node.id, node.name, node.attributes, node.data_dep_children)
         for child in node.children:
-            self.traversal_pickle(child, level + 1)
+            self.traversal(child, level + 1, monitor)
         
+    def trace_up(self, node):
+        """
+        trace up from a node, follow the data flow edge
+        """
+        pathes = []
+        nodes = [node]
+        for start in nodes:
+            nodes_group = start.data_dep_parents \
+                    if hasattr(start, 'data_dep_parents') else []
+
+            stack = [(start, iter(nodes_group))]
+            while stack:
+                parent, children = stack[-1]
+                try:
+                    child = next(children)
+                    if child not in stack:
+                        nodes_group = start.data_dep_parents \
+                                if hasattr(child, 'data_dep_parents') else []
+                        stack.append((child.extremity, iter(nodes_group)))
+                        if len(nodes_group) == 0:
+                            pathes.append([node[0] for node in stack])
+                            print(stack)
+                except StopIteration:
+                    stack.pop()
+        return pathes
+
+    def check_sink(self, node):
+        print('parents:+++++++++', node.name, node.attributes, node.data_dep_parents)
+        if hasattr(node, 'name'):
+            if node.name == 'ExpressionStatement':
+                if node.data_dep_parents is not None:
+                    for dpp in node.data_dep_parents:
+                        print (dpp.extremity.id)
+            elif node.name == 'CallExpression':
+                parent_node = node.parent
+                pathes = self.trace_up(parent_node)
+                for path in pathes:
+                    print("======================")
+                    for n in path:
+                        print("--------------------")
+                        self.traversal(n, 0, monitor=self.check_input)
+                    print([n.attributes for n in path])
+            else:
+                pass
+                # print(node.id, node.name, node.attributes, node.data_dep_children)
+
+    def check_input(self, node):
+        print(node.id, node.name, node.attributes)
+
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-f', '--input_dir')
     args = argparser.parse_args()
     input_dir_path = args.input_dir
-    loader = Loader(input_dir_path)
+    dfg_generator = DFG_generator(input_dir_path)
 
 
 main()
--- pdg_generation/test/test.js
+++ pdg_generation/test/test.js
@@ -1,5 +1,8 @@
+var cp = require("child_process");
 function foo() {
   var a = 123;
   var b = a;
   console.log(b);
+  cp.exec(b);
 }
+module.exports = {foo: foo};
